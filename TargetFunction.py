791953691636e112d721f179402387c91c562d45
koehnden <koehnden@hu-berlin.de>
2016-05-29 22:12:01 +0200

    simple sklearn implementation of AUC

--- TargetFunction.py
+++ TargetFunction.py
@@ -2,5 +2,13 @@ __author__ = 'D059348'
 
 class TargetFunction:
 
-    def getAUC(self, output, Ys):
-        return 0
+    def getAUC(self,prediction,labels):
+        
+        # import metric functions
+        from sklearn.metrics import roc_curve, auc # maybe import outside of function or class?
+        
+        # get False Positive and True Positive Rate using roc_curve 
+        fpr, tpr, thresholds = roc_curve(labels, prediction)
+        # compute the AUC and return it
+        return auc(fpr, tpr)
+
