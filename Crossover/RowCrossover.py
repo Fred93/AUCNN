c18b92ec036069d4ff926b36fa5f8866daf1311c
maxand22 <2292btsk!>
2016-06-09 20:03:52 +0200

    CoulmCrossover objektorientiert gestaltet sowie der Terminologie angepasst

--- Crossover/RowCrossover.py
+++ Crossover/RowCrossover.py
@@ -27,11 +27,12 @@ class ColumCrossover(AbstractCrossover.AbstractCrossover):
 
         i = 0
 
-        while i < (len(newPopulation) - 1):  # -1 um bei ungeraden row matrizen die letzte Einheit stehen zu lassen
+        while i < (len(newPopulation) - 1):  # -1 to take care of the last cromosome of a uneven population
 
             randompoint = rand.randint(0, newPopulation[i][0].shape[0]) #creates for each pair of chromosomes a new random number, also be possible to be 0
             exchangerow(newPopulation, i, randompoint) #calls exchangerow function to crossover the first level matrix and second level matrix of a cromosome pair
             i = i + 2 #jumps to the next pair
 
+        print('Da Rowcroshi isch au durch, gel')  # and is printing an over message
         return newPopulation #after all chromosome pairs are crossovered, the function returns the newPopulation
-        print('Da Rowcroshi isch au durch, gel') #and is printing an over message
+
