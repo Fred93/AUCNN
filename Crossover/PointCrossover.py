a28863feb8126e1744da503c8a14177ebafb1260
maxand22 <maxand@live.de>
2016-06-15 22:45:04 +0200

    Crossover, firstLevel and secondLevelMatrix

--- Crossover/PointCrossover.py
+++ Crossover/PointCrossover.py
@@ -10,27 +10,29 @@ def exchangepoint(newPopulation, i, randompoint, nrows, ncolum):
 
     #crossover for each chromosome pair attributes up to the randompoint of first level matrix
     #reshape into matrix with 1 row
-    newPopulation[i][0] = np.reshape(newPopulation[i][0], nrows * mcolum)
-    newPopulation[i + 1][0] = np.reshape(newPopulation[i + 1][0], nrows * mcolum)
+    newPopulation[i].firstLevelMatrix() = np.reshape(newPopulation[i].firstLevelMatrix(), nrows * mcolum) # [0] = firstLevelMatrix()
+    newPopulation[i + 1].firstLevelMatrix() = np.reshape(newPopulation[i + 1].firstLevelMatrix(), nrows * mcolum)
     #crossover up to the random points of the first level matrix
-    zwischenspeicher = np.matrix(newPopulation[i][0][:,0:randompoint])
-    newPopulation[i][0][:, 0:randompoint] = newPopulation[i+1][0][:,0:randompoint]
-    newPopulation[i+1][0][:, 0:randompoint] = zwischenspeicher
+    zwischenspeicher = np.matrix(newPopulation[i].firstLevelMatrix([:,0:randompoint]))
+    newPopulation[i].firstLevelMatrix([:, 0:randompoint]) = newPopulation[i+1].firstLevelMatrix([:,0:randompoint])
+    newPopulation[i+1].firstLevelMatrix([:, 0:randompoint]) = zwischenspeicher
     #reshape matrix with 1 row into original shape
-    newPopulation[i][0]=newPopulation[i][0].reshape((nrows, ncolum))
-    newPopulation[i+1][0]=newPopulation[i+1][0].reshape((nrows, ncolum))
+    newPopulation[i].firstLevelMatrix()=newPopulation[i].firstLevelMatrix().reshape((nrows, ncolum))
+    newPopulation[i+1].firstLevelMatrix()=newPopulation[i+1].firstLevelMatrix().reshape((nrows, ncolum))
 
     # crossover for each chromosome pair attributes up to the randompoint of second level matrix
     # reshape into matrix with 1 row
-    newPopulation[i][1] = np.reshape(newPopulation[i][1], nrows * mcolum)
-    newPopulation[i + 1][1] = np.reshape(newPopulation[i + 1][1], nrows * mcolum)
+
+    #[1]
+    newPopulation[i].secondLevelMatrix() = np.reshape(newPopulation[i].secondLevelMatrix(), nrows * mcolum)
+    newPopulation[i + 1].secondLevelMatrix() = np.reshape(newPopulation[i + 1].secondLevelMatrix(), nrows * mcolum)
     # crossover up to the random points of the second level matrix
-    zwischenspeicher = np.matrix(newPopulation[i][1][:, 0:randompoint])
-    newPopulation[i][1][:, 0:randompoint] = newPopulation[i + 1][1][:, 0:randompoint]
-    newPopulation[i + 1][1][:, 0:randompoint] = zwischenspeicher
+    zwischenspeicher = np.matrix(newPopulation[i].secondLevelMatrix([:, 0:randompoint]))
+    newPopulation[i]secondLevelMatrix([:, 0:randompoint]) = newPopulation[i + 1].secondLevelMatrix([:, 0:randompoint])
+    newPopulation[i + 1].secondLevelMatrix([:, 0:randompoint]) = zwischenspeicher
     # reshape matrix with 1 row into original shape
-    newPopulation[i][1]=newPopulation[i][1].reshape((nrows, ncolum))
-    newPopulation[i+1][1]=newPopulation[i + 1][1].reshape((nrows, ncolum))
+    newPopulation[i].secondLevelMatrix()=newPopulation[i]secondLevelMatrix().reshape((nrows, ncolum))
+    newPopulation[i+1].secondLevelMatrix()=newPopulation[i + 1].secondLevelMatrix().reshape((nrows, ncolum))
 
     return newPopulation
 
@@ -45,10 +47,10 @@ class PointCrossover(AbstractCrossover.AbstractCrossover):
 
         while i < (len(newPopulation) - 1):  # -1 um bei ungeraden row matrizen die letzte Einheit stehen zu lassen
 
-            randompoint = rand.randint(0, newPopulation[i][0].shape[0])
+            randompoint = rand.randint(0, newPopulation[i].firstLevelMatrix().shape[0])
 
-            nrows = newPopulation[i][0].shape[0] #is to cash the original size of the matrix
-            mcolum = newPopulation[i][0].shape[1] #is to cash the original size of the matrix
+            nrows = newPopulation[i].firstLevelMatrix().shape[0] #is to cash the original size of the matrix
+            mcolum = newPopulation[i].firstLevelMatrix().shape[1] #is to cash the original size of the matrix
 
             exchangepoint(newPopulation, i, randompoint, nrows, mcolum)
 
