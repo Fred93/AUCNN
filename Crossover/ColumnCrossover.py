66f0c2765f91ac820af50a39b5e94e17b7a207fa
maxand22 <maxand@live.de>
2016-06-26 13:03:35 +0200

    Bugs fixed in crossovers

--- Crossover/ColumnCrossover.py
+++ Crossover/ColumnCrossover.py
@@ -18,19 +18,19 @@ class ColumnCrossover(AbstractCrossover.AbstractCrossover):
         while i < (len(newPopulation) - 1):   #-1 to take care of the last cromosome of a uneven population
 
             randompoint = self.rand.randint(0, newPopulation[i].firstLevelMatrix.shape[1])  # creates for each pair of chromosomes a new random number, also be possible to be 0
-            randompointtwo = self.rand.randint(0, newPopulation[i].secondLevelMatrix.shape[1])
+            randompointscond = self.rand.randint(0, newPopulation[i].secondLevelMatrix.shape[1])
 
-            self.exchangecolum(newPopulation, i, randompoint, randompointtwo)  # calls exchangerow function to crossover the first level matrix and second level matrix of a cromosome pair
+            self.exchangecolum(newPopulation, i, randompoint, randompointscond)  # calls exchangerow function to crossover the first level matrix and second level matrix of a cromosome pair
 
             i = i + 2  # jumps to the next pair
 
         return newPopulation  # after all chromosome pairs are crossovered, the function returns the newPopulation
 
 
-    def exchangecolum(self, newPopulation, i, randompoint, randompointtwo): #function to crossover colums of chromosomes
+    def exchangecolum(self, newPopulation, i, randompoint, randompointsecond): #function to crossover colums of chromosomes
 
         CrossoverIndices = random.sample(range(0, newPopulation[i].firstLevelMatrix.shape[1]), randompoint)
-        CrossoverIndices2 = random.sample(range(0, newPopulation[i].secondLevelMatrix.shape[1]), randompointtwo)
+        CrossoverIndices2 = random.sample(range(0, newPopulation[i].secondLevelMatrix.shape[1]), randompointsecond)
 
         #crossover for first level matrix (input to hidden layer)
         zwischenspeicher = np.matrix(newPopulation[i].firstLevelMatrix[:, CrossoverIndices])
