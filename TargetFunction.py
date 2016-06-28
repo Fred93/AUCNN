0f95f227479d88fa15e4a528ba6a128993cbbc44
Frederik Pahde <frederik.pahde@sap.com>
2016-06-28 08:53:45 +0200

    added comments

--- TargetFunction.py
+++ TargetFunction.py
@@ -1,12 +1,15 @@
 from sklearn.metrics import roc_curve, auc
 
 class TargetFunction:
+    '''
+    Implements the target function (AUC) of the genetic algorithm.
+    '''
 
-    def getAUC(self, prediction, labels, regularizationTerm, weightDecay=1e-5, reg=True):
+    def getAUC(self, prediction, labels, regularizationTerm, weightDecay=1e-5, regularize=True):
         # get False Positive and True Positive Rate using roc_curve 
         fpr, tpr, thresholds = roc_curve(labels, prediction)
         # compute the AUC and return it
-        if reg:
+        if regularize:
             return auc(fpr, tpr) - weightDecay * regularizationTerm
         else:
             return auc(fpr, tpr)
