a8665f901db423e0e6e6aa892446ec4916d49f85
Frederik <frederik.pahde@sap.com>
2016-06-30 16:07:21 +0200

    performance improvements

--- Chromosome.py
+++ Chromosome.py
@@ -1,5 +1,7 @@
 import numpy as np
 import random
+import timeit
+import math
 
 class Chromosome:
 
@@ -53,6 +55,10 @@ class Chromosome:
         '''
         return 1/(1+np.exp(-x))
 
+    def sigmMath(self, x):
+        return 1 / (1 + math.exp(-x))
+
+
     def calculateOutput(self, data):
         '''
         Calculates the output of the neural network given a input matrix.
@@ -63,13 +69,19 @@ class Chromosome:
         input = np.append(ones, input, axis=1)                                         #add bias unit
         hiddenLayer_input = input * self.firstLevelMatrix
 
-        hiddenLayer_output = np.mat(np.apply_along_axis(self.sigm, 0, hiddenLayer_input))
+        start = timeit.default_timer()
+        #using apply function is too expensive --> reshaping improves performance
+        n = hiddenLayer_input.shape[0]
+        m = hiddenLayer_input.shape[1]
+        vec = hiddenLayer_input.reshape((1,n*m))
+        vec = self.sigm(vec)
+        #vec = np.apply_along_axis(self.sigmMath, 0, vec)
+        hiddenLayer_output = vec.reshape((n,m))
+
         ones = np.transpose(np.mat(np.ones(hiddenLayer_output.shape[0])))
-
         hiddenLayer_output = np.append(ones, hiddenLayer_output, axis=1)               #add bias unit
-
+        #print "step2: " + str(timeit.default_timer() - start)
 
         outputLayer_input = hiddenLayer_output * self.secondLevelMatrix
         outputLayer_output = np.mat(np.apply_along_axis(self.sigm, 0, outputLayer_input))       #elementwise sigmoid
-
         return outputLayer_output
